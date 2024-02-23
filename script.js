68b7047c03f714206580cbaa4c5ab32979804d61
cgilut <117078690+cgilut@users.noreply.github.com>
2024-02-23 14:24:54 +0100

    Create 16x16 static grid using JS

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict"
+
+
+const drawingBoardDiv = document.querySelector('.drawing-board-container')
+
+function createGrid(gridSize) {
+    for (let i = 0; i <= gridSize - 1; i++) {
+        const newDiv = document.createElement('div')
+        newDiv.classList.add('row', `row-${i}`)
+        drawingBoardDiv.appendChild(newDiv)
+        const rowDiv = document.querySelector(`.row-${i}`)
+
+        for (let k = 0; k <= gridSize - 1; k++) {
+            const newDiv = document.createElement('div')
+            newDiv.classList.add('pixel')
+            rowDiv.appendChild(newDiv)
+        }
+
+    }
+};
+
+createGrid(16);
